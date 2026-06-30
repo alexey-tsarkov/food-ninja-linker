e7433a0ef050e1ed725de607aa43b8a621ff9a55
Alex Tsarkov <alex.tsarkov@gmail.com>
2026-06-30 18:31:56 +0300

    feat: add event handlers

--- app/Models/Click.php
+++ app/Models/Click.php
@@ -9,6 +9,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 #[Fillable(['link_id', 'ip_address'])]
 class Click extends Model
 {
+    public const ?string UPDATED_AT = null;
+
+    #[\Override]
+    protected static function booted(): void
+    {
+        static::created(static function (Click $click): void {
+            Link::where('id', $click->link_id)->increment('total_clicks');
+        });
+    }
+
     /**
      * @return BelongsTo<Link, self>
      */
--- app/Models/Link.php
+++ app/Models/Link.php
@@ -6,10 +6,27 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 #[Fillable('user_id', 'short_code', 'original_url')]
 class Link extends Model
 {
+    public const ?string UPDATED_AT = null;
+
+    #[\Override]
+    protected static function booted(): void
+    {
+        static::creating(static function (Link $link): void {
+            $link->user_id = Auth::id();
+            $link->short_code ??= self::generateShortCode();
+        });
+    }
+
+    protected static function generateShortCode(int $size = 4): string
+    {
+        return \rtrim(\strtr(\base64_encode(\random_bytes($size)), '+/', '-_'), '=');
+    }
+
     /**
      * @return BelongsTo<User, self>
      */
