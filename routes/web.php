38431bdd765c551794a49571ad006c16add8a820
Alex Tsarkov <alex.tsarkov@gmail.com>
2026-06-30 21:05:41 +0300

    feat: add click action

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,23 @@
 <?php
 
+use App\Models\Link;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/admin');
+
+Route::get('/{short_code}', static function (Request $request) {
+    /** @var Link */
+    $link = Link::where('short_code', $request->short_code)->firstOrFail();
+
+    $link->clicks()->create([
+        'ip_address' => $request->getClientIp() ?? '0.0.0.0',
+    ]);
+
+    if (App::hasDebugModeEnabled()) {
+        return response($link->original_url);
+    }
+
+    return redirect($link->original_url);
+})->name('click');
