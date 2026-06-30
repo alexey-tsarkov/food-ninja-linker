415668c7996cc52c970b4ea87eb9d7370bb878e7
Alex Tsarkov <alex.tsarkov@gmail.com>
2026-06-30 19:04:19 +0300

    feat: link resource panel

---
+++ app/Filament/Resources/LinkResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LinkResource\Pages;
+use App\Models\Link;
+use App\Models\Scopes\AuthUserScope;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class LinkResource extends Resource
+{
+    protected static ?string $model = Link::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-link';
+
+    #[\Override]
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->tap(new AuthUserScope);
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('original_url')
+                    ->label('Original URL')
+                    ->required()
+                    ->maxLength(255)
+                    ->url()
+                    ->columnSpan('full'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('short_code')
+                    ->label('Code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('original_url')
+                    ->label('Original URL')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('total_clicks')
+                    ->label('Clicks')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created at')
+                    ->date()
+                    ->dateTimeTooltip()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLinks::route('/'),
+        ];
+    }
+}
